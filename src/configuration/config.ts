873720ff4bba79ec75dd9023d17fea1abd79c256
ajeet <ajeetx@email.com>
2020-03-21 08:03:17 +1100

    updated

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -1,6 +1,6 @@
 import {environment} from '../environments/environment'
 export const Config= {
-    baseUrl:environment.production?"https://seven11api.herokuapp.com/":"https://localhost:5001/",
+    baseUrl:environment.production?"https://saimex-api.herokuapp.com/":"https://localhost:5001/",
     // ipaddressurl:"https://api.ipify.org?format=json",
     ipaddressurl:environment.production?"https://api.ipify.org?format=json":"http://httpbin.org/ip",
     geoLocation:"https://ipapi.co/"
